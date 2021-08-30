07124a3c7c2e17e8236054b7e0b30edfb09bd163
Fernando Santis <fpsantis@gmail.com>
2021-08-30 00:55:13 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/pedrapapeltesoura/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.scl.sdm.pedrapapeltesoura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
