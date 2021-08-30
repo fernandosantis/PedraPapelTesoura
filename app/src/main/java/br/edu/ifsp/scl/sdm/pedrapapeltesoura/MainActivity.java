d341eaf3c7716f14018d6df954395e9dc9eb8a27
Fernando Santis <fpsantis@gmail.com>
2021-08-30 04:12:03 -0300

    Layout Pronto

--- app/src/main/java/br/edu/ifsp/scl/sdm/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/pedrapapeltesoura/MainActivity.java
@@ -2,13 +2,28 @@ package br.edu.ifsp.scl.sdm.pedrapapeltesoura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+
+import br.edu.ifsp.scl.sdm.pedrapapeltesoura.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Binding
+    ActivityMainBinding binding;
+    Integer qtdOponentes = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Inflando o Layout
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
     }
+
+
 }
